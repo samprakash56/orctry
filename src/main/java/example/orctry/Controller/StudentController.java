c84c68f779af93b766a675ef4b9187c4864e64a0
samprakash56 <samprakash27032004@gmail.com>
2025-11-09 15:32:43 +0530

    pushed

--- src/main/java/example/orctry/Controller/StudentController.java
+++ src/main/java/example/orctry/Controller/StudentController.java
@@ -4,14 +4,13 @@ import example.orctry.Entity.Student;
 import example.orctry.Service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
+
 
 @RestController
 public class StudentController {
